d32452c1d09f95d9b2da5e9d8c680277a4d73c07
Raj Nigam <rajington@users.noreply.github.com>
2018-03-13 18:43:30 -0400

    Make `locales` argument optional

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare class IntlMessageFormat {
-    constructor(message: string, locales: string | string[], formats?: any);
+    constructor(message: string, locales?: string | string[], formats?: any);
     format(context?: any): string;
     static defaultLocale: string;
 }
